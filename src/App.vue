652bcfc45e4a150c6119735361de274462829b2a
d1nker <qfaure@gmail.com>
2020-06-12 19:09:46 +0200

    testing 3.0.0-beta.1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <counter></counter>
   </div>
 </template>
+
+<script>
+import Counter from './components/Counter.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Counter,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="increment">
+    Count: {{ state.count }} Double {{ state.double }}
+  </button>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+    });
+    function increment() {
+      state.count++;
+    }
+    return { state, increment };
+  },
+};
+</script>
